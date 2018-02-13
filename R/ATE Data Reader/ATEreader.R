be48e07b7fb75d0f9be5fb8d831cebf4eb47822f
McNibbler <31897387+McNibbler@users.noreply.github.com>
2018-02-13 16:12:04 -0500

    bleh this is going to be a nightmare but here's some progress weow

--- R/ATE Data Reader/ATEreader.R
+++ R/ATE Data Reader/ATEreader.R
@@ -5,6 +5,10 @@
 # February 12, 2018                #
 # Thomas Kaunzinger                #
 # XCerra Corp.                     #
+#                                  #
+# References:                      #
+# Shiny + shinyFiles Libraries     #
+# RADAR Library                    #
 ####################################
 
 # Libraries
@@ -21,7 +25,7 @@ source("Radar.r")
 #tclvalue(.Radar$.TkRadar.env$default_min_plots_per_page) <- 8
 #tclvalue(.Radar$.TkRadar.env$default_use_csv_formulas) <- 1
 #tclvalue(.Radar$.TkRadar.env$default_use_OOCalc_csv) <- 1
-#tclvalue(.Radar$.TkRadar.env$default_add_normal_curve) <- 1
+#tclvalue(.Radar$.TkRadar.env$default_add_no4rmal_curve) <- 1
 
 setwd(my.dir)
 rm(my.dir)
@@ -31,4 +35,109 @@ rm(my.dir)
 
 
 # Testing with this file
-data.file <- file("./Data/super secret test data.std")
+data.dir <- "./Data/"
+data.name <- "super secret test data.std"
+results.name <- "./Data/RDTF/Rtdf Results.rtdf"
+
+#data.rtdf <- ConvertStdf(stdf_name = data.name, rtdf_name = results.name, stdf_dir = data.dir)
+
+
+# Client side code
+ui <- fluidPage(
+  
+  # Title
+  titlePanel("ATE STDF/CSV Processor"),
+  
+  
+  sidebarLayout(
+    
+    # Uploading the file
+    sidebarPanel(
+      
+      fileInput("file1", h3("STDF/CSV File"),
+                accept = c(".std", "text/csv", "text/comma-separated-values,text/plain", ".csv")
+      )
+      
+    ),
+    
+    mainPanel(
+      textOutput("filepath1"),
+      textOutput("filepath2")
+    )
+    
+  )
+  
+)
+
+
+# Server side calculations
+server <- function(input, output){
+  
+  # Jesus Christ STDF files are huge
+  # Changed size limit from default 5MB to 50MB
+  options(shiny.maxRequestSize=50*1024^2) 
+  
+  
+  # Returns the parameters for the file when it's eventually uploaded
+  my.file <- reactive({
+    
+    if (is.null(input$file1)){
+      return(NULL)
+    }
+    
+    input$file1
+  
+  })
+  
+  
+  # Finds ONLY the filepath with no file (stupid quirk of RADAR needs this I don't know why)
+  data.dir <- reactive({
+    
+    file1 <- toString(my.file()[4])
+    
+    list1 <- unlist(strsplit(file1,"/"))[0:((length(unlist(strsplit(file1,"/")))) - 1)]
+
+    paste(list1, collapse = '/')
+    
+  })
+  
+  
+  # Just to make sure I have the filepath actually working. This won't stay for long.
+  output$filepath1 <- renderText({
+    
+    if (is.null(my.file())){
+      return(NULL)
+    }
+    
+    paste("Your temp filepath is: ", data.dir())
+    
+  })
+  
+  
+  # Same thing with the file name
+  output$filepath2 <- renderText({
+    
+    if (is.null(my.file())){
+      return(NULL)
+    }
+    
+    
+    paste("Your file is: ", my.file()[1])
+    
+    
+  })
+  
+  
+  # Jesus I don't know what I'm gonna do here but let's roll with it I guess
+  data.rtdf <- reactive({
+    
+    ConvertStdf(stdf_name = my.file()[1], rtdf_name = "data.rtdf", stdf_dir = data.dir())
+    
+  })
+  
+  
+}
+
+
+# Launches the Application
+shinyApp(ui, server)
