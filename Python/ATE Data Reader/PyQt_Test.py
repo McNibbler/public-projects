ff794bf66ab5512a7ff884519b19e1188b2e5f92
McNibbler <31897387+McNibbler@users.noreply.github.com>
2018-03-16 10:10:42 -0400

    learning how to a design a UI now so that I can implement it in the ATE reader. may or may not just start a new file for organization purposes, but I'm undecided.

--- Python/ATE Data Reader/PyQt_Test.py
+++ Python/ATE Data Reader/PyQt_Test.py
@@ -1,23 +1,44 @@
 # Probably not doing anything meaningful here, but I want to test how pyqt works over tkinter
+# Credits to ZetCode for the tutorial
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 
-class App(QMainWindow):
+class Application(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = "Rn im literally following a tutorial lmao"
-        self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
+        self.main_window()
+
+    def main_window(self):
+
+        # Default UI elements
+        WINDOW_LOCATION = (100, 100)
+        WINDOW_SIZE = (300, 200)
+        FONT = 'Raleway'
+        FONT_SIZE = 12
+
+        QToolTip.setFont(QFont(FONT, FONT_SIZE))
+
+        upload_button = QPushButton('Upload STD/STDF', self)
+        upload_button.setToolTip('Browse your files for a file ending in .std or .stdf')
+        upload_button.resize(upload_button.sizeHint())
+        upload_button.move(50, 50)
+
+        self.setGeometry(WINDOW_LOCATION[0], WINDOW_LOCATION[1], WINDOW_SIZE[0], WINDOW_SIZE[1])
+        self.setWindowTitle('ATE Data Reader')
         self.show()
 
+    def upload_event(self, event):
 
-if __name__ == 'main':
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+
+    nice = Application()
+
     sys.exit(app.exec_())
